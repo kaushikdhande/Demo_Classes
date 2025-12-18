14bba5fe1dc3c744755a2230da93aea77ca38a2e
kaushikdhande <kaushikdhande06@gmail.com>
2025-12-18 14:43:45 +0530

    first commit in eclipse

---
+++ src/core_java_object_oriented/Practice_Demo_Trail.java
@@ -0,0 +1,52 @@
+package core_java_object_oriented;
+
+public class Practice_Demo_Trail {
+	public static void main(String[] args) {
+//		int _x=10;
+//		int x___y=10;						allow
+//		int a=10__000;						allow
+//		float e=10_1.5f;  					allow
+//		System.out.println(e);
+//		System.out.println(a);
+//		//char #xy=10;               not allowed variable declaration
+//		System.out.println(_x);
+//		System.out.println(x___y);
+		
+//		                                 type casting
+		
+//	                                     1.Implicitly 
+		
+//		byte a=10;
+//		int b=a;
+//		System.out.println(a);
+//		System.out.println(b);
+		 
+//                                        2. Explicityly;
+		
+//		double a=25.17;
+//		int b;
+//		b=(int)a;
+//		System.out.println(a);
+//		System.out.println(b);
+
+//		float a=(float)10.5;
+//		long a=10;
+//		long b=214783648;
+//		System.out.println(b);
+//
+//		System.out.println(a);
+//
+//		System.out.println(a);
+//
+//											example
+		
+//		int c=5;
+//		int d=2;
+//		double e=c/d;					read from right side of = 
+//		System.out.println(e); //2.0
+		
+//												primitive datatypes
+
+	}
+}
+ 
